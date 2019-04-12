ab1ffd50bfef0ffb5fc241b5d83b54924d565593
SofiBit <sofiabitno@gmail.com>
2019-04-13 01:25:27 +0300

    add correct for answers

--- db/migrate/20190412213447_create_answers.rb
+++ db/migrate/20190412213447_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.text :body
+      t.boolean :correct, null: false
       t.references(:question, foreign_key: true, index: true)
 
       t.timestamps
