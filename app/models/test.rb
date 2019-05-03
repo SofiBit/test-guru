e36bf50b84aa5fa3e6cffde74c41958cbf4ebc7a
SofiBit <sofiabitno@gmail.com>
2019-05-03 20:09:45 +0300

    array of test names

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+
+  def self.with_category(category_title)
+    tests_title = []
+    Test.joins(:category).order(title: :desc).where(categories: {title: category_title}).each do |test|
+      tests_title << test.title
+    end
+    tests_title
+  end
 end
