2f42e0500803cb110231315e6721d603e032352c
SofiBit <sofiabitno@gmail.com>
2019-05-07 03:38:34 +0300

    method Test with category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+
+  def self.with_category(title)
+    tests = Test.joins("JOIN categories ON tests.category_id = categories.id")
+    .where("categories.title >= ?", title).order(title: :desc)
+    tests.pluck(:title)
+  end
 end
