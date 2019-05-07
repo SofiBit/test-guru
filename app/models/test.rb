680424aa4e25e6f8408db52be9a35a72e551f9e5
SofiBit <sofiabitno@gmail.com>
2019-05-07 03:38:34 +0300

    method Test with category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.with_category(title)
+    joins("JOIN categories ON tests.category_id = categories.id")
+    .where("categories.title = ?", title).order(title: :desc).pluck(:title)
+  end
 end
