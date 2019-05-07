adfd58e206ef6094045fa9cba7479497f5a6f21d
SofiBit <sofiabitno@gmail.com>
2019-05-07 03:46:53 +0300

    method user tests with level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  def tests(level)
+    Test.joins("JOIN passed_tests ON passed_tests.test_id = tests.id")
+       .where("tests.level = ? AND passed_tests.user_id = ?", level, id)
+  end
 end
