785e69dd613c14db5584eed8f73d1869fabe1592
SofiBit <sofiabitno@gmail.com>
2019-05-08 16:45:26 +0300

    method Test with category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.with_category(title)
+    tests = Test.joins("JOIN categories ON tests.category_id = categories.id")
+    .where("categories.title = ?", title)
+    tests.pluck(:title)
+  end
 end
