b81f885dfc85088a21a59d689847c92584ba601a
SofiBit <sofiabitno@gmail.com>
2019-05-10 23:36:56 +0300

    associations in models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :category
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
 
   def self.with_category(title)
     joins("JOIN categories ON tests.category_id = categories.id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :author_tests, class_name: 'Test'
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
 
   def tests(level)
     Test.joins("JOIN passed_tests ON passed_tests.test_id = tests.id")
