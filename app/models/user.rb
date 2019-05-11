6f00457a800d752656d536063edf371d8d2d9f45
SofiBit <sofiabitno@gmail.com>
2019-05-11 22:33:34 +0300

    change methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :tests, through: :passed_tests, dependent: :destroy
 
   def tests_with_level(level)
-    Test.joins("JOIN passed_tests ON passed_tests.test_id = tests.id")
-       .where("tests.level = ? AND passed_tests.user_id = ?", level, id)
+    tests.where(level: level)
   end
 end
