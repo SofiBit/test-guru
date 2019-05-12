8d3951e245216ed1a848c5cc093b6f4d9d0366fe
SofiBit <sofiabitno@gmail.com>
2019-05-12 18:11:40 +0300

    add scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :right, -> { where(correct: true) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,13 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :users, through: :passed_tests, dependent: :destroy
 
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :average_level, -> { where(level: 2..4) }
+  scope :difficult_level, -> { where(level: 5..Float::INFINITY) }
+  scope :category_title, ->(title) { joins(:category).where(categories: { title: title }) }
+  scope :sort_title, -> { order(title: :desc) }
+
   def self.with_category(title)
-    joins(:category).where(categories: {title: title}).order(title: :desc).pluck(:title)
+    category_title(title).sort_title.pluck(:title)
   end
 end
