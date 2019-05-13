330881643a1abcd9e352b3babc97323084c68993
SofiBit <sofiabitno@gmail.com>
2019-05-13 08:31:34 +0300

    edits

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,9 +4,4 @@ class Question < ApplicationRecord
 
   validates :body, presence: true
   validate :quantity_answers
-
-  def quantity_answers
-    return if [1, 2, 3, 4].include? answers.count
-    errors.add(:answers, "have a wrong quantity")
-  end
 end
