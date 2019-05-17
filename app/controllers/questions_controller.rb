ecc42c93477c3967997ea485aee3365642412477
SofiBit <sofiabitno@gmail.com>
2019-05-17 18:02:14 +0300

    comments befor_action find_test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy]
+  #before_action :find_test, only: %i[index create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -30,6 +31,10 @@ class QuestionsController < ApplicationController
   def find_question
     @question = Question.find(params[:id])
   end
+
+  # def find_test
+  #   @test = Test.find(params[:id])
+  # end
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
