2f0b47bdeaed8358d98b02e3d090a19344d813c1
SofiBit <sofiabitno@gmail.com>
2019-05-17 04:24:34 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'questions#index'
+  # Ресурс вопросов, вложенный в ресурс тестов
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
