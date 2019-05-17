02078380a91ebe965001826beeec7b99ab68e784
SofiBit <sofiabitno@gmail.com>
2019-05-17 17:52:09 +0300

    show and delete questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,20 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[show destroy]
+
   def index
      render inline: '<%= Question.all.pluck(:body) %>'
+  end
+
+  def show; end
+
+  def destroy
+    @question.destroy
+    render plain: 'Question deleted'
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
