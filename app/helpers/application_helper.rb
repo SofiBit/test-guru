05474cd9cf1e7a47911a302e306460c43bdd18cd
SofiBit <sofiabitno@gmail.com>
2019-06-04 02:10:53 +0300

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to "Project #{repo}", "https://github.com/#{author}/#{repo}"
+  end
 end
