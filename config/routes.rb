a35789c17ecfcd84460221683a7166911d5ccb3c
SofiBit <sofiabitno@gmail.com>
2019-06-06 15:52:37 +0300

    answers views through scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :answers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'questions#index'
   # Ресурс вопросов, вложенный в ресурс тестов
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
