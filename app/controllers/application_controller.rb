ac38183e253c701f01b1da620efd47bb8a91e546
София <sofiabitno@gmail.com>
2019-12-05 22:40:42 +0200

    finish localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = I18n.locale_available?(params[:locale]) ? params[:locale] : I18n.default_locale
   end
 
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
+  def default_url_options
+    { locale: (I18n.locale if I18n.locale != I18n.default_locale) }
   end
 
   def configure_permitted_parameters
